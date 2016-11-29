93dd589650e234ba3d039f05274beb49eef8e15a
billywood8 <g.wood@bssdstd.net>
2016-11-29 13:18:14 -0600

    Activity138- second commit

--- Activity138/billywood138.py
+++ Activity138/billywood138.py
@@ -2,23 +2,22 @@ from __future__ import print_function
 import random
 
 def guess_winner(players):
-    '''Summarize the function in this docstring.
+    '''The function in this docstring is for the user to be guess the winner of the lottery
     
-    Provide descriptions for the arguments and say what type each one is.
-    Describe the type and meaning of the value returned.
+    The arguement in the code is players.It is a tuple.
+    A tuple is a list that cannot be changed.
     '''
     winner = random.choice(players) 
 
     ####
-    # Summarize the following section of code here
+    # The winner is chosee at random for the users list of input.
     ####
     print('Guess which of these people won the lottery: ',end='')
     for p in players:
         print(p+', ', end='')
 
     ####
-    # Summarize the following section of code here
-    ####
+    # It checks if the users choice was right or wrong. If right, it will say thank you and if wrong it will say guess again!
     guesses = 1 
     while raw_input() != winner:
         print('Guess again!')
